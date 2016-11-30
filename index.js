96359312e474462709aa0a27d62c9951b8f59617
agate <agate.hao@gmail.com>
2016-11-30 10:55:53 +0800

    expanded server timeout to 1hour

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var http = require('http');
 var url = require('url');
 var qs = require('qs');
 
+var MAX_TIME_OUT = 1000 * 60 * 60;
+
 var server = http.createServer(function (req,res) {
   var requestAt = new Date();
   var parsedUrl = url.parse(req.url);
@@ -12,10 +14,15 @@ var server = http.createServer(function (req,res) {
   setTimeout(function () {
     res.end(JSON.stringify({
       requestAt: requestAt,
-      responseAt: new Date()
+      responseAt: new Date(),
+      info: "MAX TIMEOUT IS " + MAX_TIME_OUT + " MSECS."
     }));
   }, timeout);
 });
 
 server.listen('3000');
 console.log("listen on localhost:3000");
+
+server.setTimeout(MAX_TIME_OUT, function () {
+  console.log("HIT TIMEOUT")
+})
